ed95f1851be4648f026d1790a02520415ae8b421
MatNepo <71396245+MatNepo@users.noreply.github.com>
2023-01-08 17:18:56 +0300

    6th lesson: bestHighJump and last task (computeDeviceCells) completed

--- src/lesson6/task1/Parse.kt
+++ src/lesson6/task1/Parse.kt
@@ -2,6 +2,8 @@
 
 package lesson6.task1
 
+import java.util.*
+
 // Урок 6: разбор строк, исключения
 // Максимальное количество баллов = 13
 // Рекомендуемое количество баллов = 11
@@ -49,7 +51,7 @@ fun timeSecondsToStr(seconds: Int): String {
  */
 fun main() {
     println("Введите время в формате ЧЧ:ММ:СС")
-    val line = readLine()
+    val line = readlnOrNull()
     if (line != null) {
         val seconds = timeStrToSeconds(line)
         if (seconds == -1) {
@@ -127,7 +129,35 @@ fun bestLongJump(jumps: String): Int = TODO()
  * При нарушении формата входной строки, а также в случае отсутствия удачных попыток,
  * вернуть -1.
  */
-fun bestHighJump(jumps: String): Int = TODO()
+fun bestHighJump(jumps: String): Int {
+    val result = try {
+        val jumpsList = jumps.split(" ") // create list with jumps and info abt them separated
+        /*
+          220
+          +
+          224
+          %+
+          228
+          %-
+          230
+          +
+          232
+          %%-
+          234
+          %
+        */
+        var maxJump = 0 // reset maxJump
+        for (current in jumpsList.indices step 2) { // go through jumpsList with step 2
+            if (jumpsList[current + 1].contains('+') && jumpsList[current].toInt() > maxJump) {
+                maxJump = jumpsList[current].toInt()
+            }
+        }
+        maxJump
+    } catch (e: Exception) {
+        -1 // smth wrong or no gd tries -> return -1
+    }
+    return result // return try result of maxJump
+}
 
 /**
  * Сложная (6 баллов)
@@ -136,7 +166,7 @@ fun bestHighJump(jumps: String): Int = TODO()
  * использующее целые положительные числа, плюсы и минусы, разделённые пробелами.
  * Наличие двух знаков подряд "13 + + 10" или двух чисел подряд "1 2" не допускается.
  * Вернуть значение выражения (6 для примера).
- * Про нарушении формата входной строки бросить исключение IllegalArgumentException
+ * При нарушении формата входной строки бросить исключение IllegalArgumentException
  */
 fun plusMinus(expression: String): Int = TODO()
 
@@ -213,4 +243,79 @@ fun fromRoman(roman: String): Int = TODO()
  * IllegalArgumentException должен бросаться даже если ошибочная команда не была достигнута в ходе выполнения.
  *
  */
-fun computeDeviceCells(cells: Int, commands: String, limit: Int): List<Int> = TODO()
+// this sub function will be needed later in the main part of computeDeviceCells() fun
+fun endOfCurrentLoop(str: String): Int {
+    var loopCommands = 0
+    // endCurrentLoopCounter - value with a position of the element which ends current loop
+    for (endCurrentLoopCounter in str.indices) { /* f.e., 1) [>]>[<] i = : 0 -> 2 -> return i
+                                      2) [>[<<]] i = : 0 -> 2 -> 5 -> 6 -> return i */
+        when (str[endCurrentLoopCounter]) { /* f.e., 1) [>]>[<] loopCommands = : 0 -> 1 -> 0
+                                 2) [>[<<]] loopCommands = : 0 -> 1 -> 2 -> 1 -> 0 */
+            '[' -> loopCommands++ // current loop starts
+            ']' -> if (loopCommands == 1) { // current loop ended
+                return endCurrentLoopCounter // end of current sub fun, return result to main part
+            } else loopCommands--
+        }
+    }
+    return -1 // just value to return which will never be reached cause of loopBeginEnd check
+}
+
+fun computeDeviceCells(cells: Int, commands: String, limit: Int): List<Int> {
+    val availableChars = setOf('>', '<', '+', '-', '[', ']') // available alphabet
+    // to make easier working with chars, remove spaces, saving on;y useful chars
+    val withoutSpaces = commands.filter { it != ' ' } // if it's not a space -> add to
+
+    // get started with checking that every "[" has a pair like "]"
+    var loopBeginEnd = 0 // needed to check that all loops, which started, ended
+    for (char in withoutSpaces) when (char) { /* f.e., 1) [>[<<]] loopBeginEnd = : 0-> 1 -> 2 -> 1 -> 0
+                                                       2) [>]<[>] loopBeginEnd = : 0 -> 1 -> 0 -> 1 -> 0
+                                                       3) [[>>[<]>] loopBeginEnd = : 0 -> 1 -> 2 -> 3 -> 2 -> 1
+                                                       4) >[>[q]] loopBeginEnd = : 0 -> 1 -> 2 -> exception */
+        !in availableChars /* if unavailable char met */ -> throw IllegalArgumentException() // exception catch
+        '[' -> loopBeginEnd++ // 1st element "[" of pair "[]" found, so ++
+        ']' -> loopBeginEnd-- // 2nd element "]" of pair "[]" found, so --
+    }
+    if (loopBeginEnd != 0) throw IllegalArgumentException()  /* f.e., 1) [>[<<]] loopBeginEnd = 0 -> all is gd
+                                                                      2) [>]<[>] loopBeginEnd = 0 -> all is gd
+                                                                      3) [[>>[<]>] loopBeginEnd = 1 -> exception */
+    // end of checking on pairs
+
+    // create variables and values
+    // to work with current element it's easier to use array and then convert it to List
+    val result = Array(cells) { 0 } // reset result array
+    var detector = cells / 2 // set detector on start position
+    /* I tried first to use withoutSpaces but realised that withoutSpaces can't be used
+       cause of spaces in initial commands, it'll influence on detector start position
+       f.e., if commands = "<<<    >", start detector value will be between "<<< " and "   >" but
+       for withoutSpaces the same initial commands will detect detector start between "<<" and "<>"
+     */
+    val amountOfCommands = commands.length // withoutSpaces can't be used cause of spaces in initial List
+    var currentCommand = 0
+    var currentCommandCounter = 1
+    val bufferLoopPair = Stack<Int>() // just buffer needed
+    // main part starts
+    while (currentCommand < amountOfCommands && currentCommandCounter <= limit) {
+        when (commands[currentCommand]) { // go through commands using lambda
+            '>' -> detector++ // next cell
+            '<' -> detector-- // previous cell
+            '+' -> result[detector]++ // cell value ++
+            '-' -> result[detector]-- // cell value --
+            '[' -> // beginning of possible cycle
+                if (result[detector] == 0) { // according to the initial task
+                    currentCommand += endOfCurrentLoop(commands.substring(currentCommand))
+                } else bufferLoopPair.push(currentCommand) // save loop start cell
+
+            ']' -> // end of possible cycle
+                if (result[detector] != 0) { // according to the initial task
+                    currentCommand = bufferLoopPair.peek()
+                } else bufferLoopPair.pop()
+        }
+        currentCommand++ // next command
+        currentCommandCounter++ // renew currentCommandCounter
+        /* before end or continue the cycle it's needed to check that detector
+           belongs [0, cells - 1] => [0, cells), case cells has Int type.
+           So if it doesn't belong to range -> throw IllegalStateException() */
+        if (detector !in 0 until cells) throw IllegalStateException()
+    }
+    return result.toList()
+} // interesting task, looks like realisation of Turing Machine
